3ef357065cf400edfc6b582df9475ab15e57fadd
Andy Smircich <smirciat@hotmail.com>
2026-06-30 16:14:49 -0800

    ecosystem file
    Signed-off-by: Andy Smircich <smirciat@hotmail.com>

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  apps: [
+    {
+      name: 'my-express-api',
+      // Runs using the global 'pm2' npm package (or standard npx)
+      script: 'npx',
+      // nx command to serve the api project
+      args: 'nx serve api --configuration=production',
+      // Ensure the correct Node.js version is forced
+      interpreter: '/home/andy/.nvm/versions/node/v24.16.0/bin/node', 
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 8080
+      }
+    },
+    {
+      name: 'my-angular-web',
+      script: 'npx',
+      // Serve the static build output with an HTTP server (e.g., serve or http-server)
+      // Note: Angular should first be built using `npx nx build web`
+      args: 'npx serve dist/apps/web -p 4200',
+      interpreter: '/home/andy/.nvm/versions/node/v24.16.0/bin/node',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
